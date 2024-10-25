26aa315b22ec0ff0965e91a96ff5a2bcc29a138d
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2024-10-25 15:22:35 +0800

    feat(Organization): add model to generate organization tbl

--- app/Http/Controllers/Auth/SignUpController.php
+++ app/Http/Controllers/Auth/SignUpController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OrganizationRequest;
 
 class SignUpController extends Controller
 {
     public function signUpOrg ()
     {
         return inertia('Auth/SignUpOrg');
+    }
+
+    public function saveOrg (OrganizationRequest $request)
+    {
+        dd($request);
     }
 }
