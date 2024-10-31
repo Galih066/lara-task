903f947ea42bf6284cb0193addad6b31014ecf7d
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2024-10-31 09:01:44 +0800

    feat(Login): setup for login schema

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 
 class LoginController extends Controller
 {
     public function index ()
     {
         return inertia('Auth/Login');
+    }
+
+    public function login (LoginRequest $request)
+    {
+        dd($request->all());
     }
 }
