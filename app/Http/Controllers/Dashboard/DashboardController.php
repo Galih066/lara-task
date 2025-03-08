e83c95bd0bbb14e4c210a7c6e2b4118384d3fe91
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-03-08 13:29:30 +0800

    feat(Dashboard): setup for dashboard services

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -6,20 +6,27 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Services\ProfileService;
+use App\Services\DashboardService;
 
 class DashboardController extends Controller
 {
     protected $profileService;
+    protected $dashboardSvc;
 
-    public function __construct(ProfileService $profileService)
+    public function __construct(
+        ProfileService $profileService,
+        DashboardService $dashboardSvc
+    )
     {
         $this->profileService = $profileService;
+        $this->dashboardSvc = $dashboardSvc;
     }
 
     public function index ()
     {
         $loggedUser = Auth::user();
         $user = $this->profileService->getUserWithProfile($loggedUser);
-        return inertia('Dashboard/Dashboard', compact('user'));
+        $summary = $this->dashboardSvc->getDashboardSummary($loggedUser);
+        return inertia('Dashboard/Dashboard', compact('user', 'summary'));
     }
 }
---
+++ app/Services/DashboardService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Task;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+
+class DashboardService {
+    public function getDashboardSummary ($user)
+    {
+        return $user;
+    }
+}
